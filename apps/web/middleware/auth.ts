7621d4284c220d57676fbcc0a365d2d4e2045120
emirsassan <endwse@gmail.com>
2024-07-05 10:57:16 +0300

    refactor cookie header

--- apps/web/middleware/auth.ts
+++ apps/web/middleware/auth.ts
@@ -1,12 +1,9 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
-    const tokenCookie = useCookie('token')
     // if someone has a better implementation please open a pr
     const { data: response } = await useAsyncData<{ data: string }>(
         'authKey',
         () => $fetch('http://localhost:3169/users/me', {
-            headers: {
-                cookie: `token=${tokenCookie.value}`
-            }
+            headers: useRequestHeaders(['cookie'])
         })
       )
 
