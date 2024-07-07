014090637e57885ffee373f4b196ae4a06c622ae
emirsassan <endwse@gmail.com>
2024-07-07 12:04:04 +0300

    added pagination

--- apps/web/pages/index.vue
+++ apps/web/pages/index.vue
@@ -4,6 +4,8 @@ definePageMeta({
   middleware: "auth"
 })
 
+let startIndex = ref(0)
+let endIndex = ref(10)
 
 const { data: response, error, execute } = await useAsyncData<{ data: { url: string, username: string, password: string }[] }>(
   'logins',
@@ -40,18 +42,61 @@ const { data: response, error, execute } = await useAsyncData<{ data: { url: str
           </TableRow>
         </TableHeader>
         <TableBody>
-          <TableRow v-for="data in response?.data">
+          <TableRow v-for="data in response?.data.slice(startIndex, endIndex)">
             <TableCell class="font-medium">
               {{ data?.url }}
             </TableCell>
             <TableCell>{{ data.username }}</TableCell>
             <TableCell>**********</TableCell>
             <TableCell>
-              placeholder
+              <Dialog>
+                <DialogTrigger as-child>
+                  <Button variant="outline" class="h-8">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                      stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                      class="size-4">
+                      <path d="M4 2v20l2-1 2 1 2-1 2 1 2-1 2 1 2-1 2 1V2l-2 1-2-1-2 1-2-1-2 1-2-1-2 1Z" />
+                      <path d="M14 8H8" />
+                      <path d="M16 12H8" />
+                      <path d="M13 16H8" />
+                    </svg>
+                  </Button>
+                </DialogTrigger>
+                <DialogContent class="sm:max-w-[425px]">
+                  <DialogHeader>
+                    <DialogTitle>Edit credentials</DialogTitle>
+                    <DialogDescription>
+                      Make changes to your credentials here. Click save when you're done.
+                    </DialogDescription>
+                  </DialogHeader>
+                  <p>still wip if you see this why dont you finish it and create a pr</p>
+                  <DialogFooter>
+                    <Button type="submit">
+                      Save changes
+                    </Button>
+                  </DialogFooter>
+                </DialogContent>
+              </Dialog>
             </TableCell>
           </TableRow>
         </TableBody>
       </Table>
+    </div>
+
+    <div class="fixed bottom-4 left-0 right-0 flex justify-center items-center">
+      <Pagination :total="response?.data.length" :sibling-count="1" show-edges :default-page="1">
+        <PaginationList class="flex items-center gap-1">
+          <PaginationPrev @click="() => {
+            startIndex = startIndex - 10
+            endIndex = endIndex - 10
+          }" />
+
+          <PaginationNext @click="() => {
+            startIndex = startIndex + 10
+            endIndex = endIndex + 10
+          }" />
+        </PaginationList>
+      </Pagination>
     </div>
   </div>
 </template>
